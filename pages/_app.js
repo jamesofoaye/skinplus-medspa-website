a8669e1e74f92cef584ab78a0615238a2b050882
James Ofori Ayerakwa <jamesofoaye@gmail.com>
2021-05-27 13:55:19 +0000

    homepage development

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,26 @@
-import '../styles/globals.css'
+import { ChakraProvider } from "@chakra-ui/react"
+import { extendTheme } from "@chakra-ui/react"
+
+const theme = extendTheme({
+  colors: {
+    brand: {
+      "green": "#1F3D33",
+      "olive": "#687A62",
+      "brown": "#966241",
+      "sand": "#C5ADBD",
+      "grey": "#74706D",
+      "cream": "#EFEDE8",
+      "black": "#231F20",
+    },
+  },
+})
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
 }
 
 export default MyApp
