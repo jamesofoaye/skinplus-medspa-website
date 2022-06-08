da643577d2d150389fd5ef8fef646a5752b4ed57
James Ofori Ayerakwa <58654469+jamesofoaye@users.noreply.github.com>
2022-06-08 11:33:49 +0000

    chore: added splitbee and deleted functions
    i moved functions to a different repo

--- pages/_app.js
+++ pages/_app.js
@@ -1,34 +1,38 @@
-import dynamic from "next/dynamic";
-import { ChakraProvider } from "@chakra-ui/react"
-import { extendTheme } from "@chakra-ui/react"
-import { Box } from "@chakra-ui/react"
+import dynamic from 'next/dynamic';
+import { ChakraProvider } from '@chakra-ui/react';
+import { extendTheme } from '@chakra-ui/react';
+import { Box } from '@chakra-ui/react';
 //styles for image slider
-import "../styles/styles.css"
+import '../styles/styles.css';
+import splitbee from '@splitbee/web';
 
 const theme = extendTheme({
   colors: {
     brand: {
-      "green": "#1F3D33",
-      "olive": "#687A62",
-      "brown": "#966241",
-      "sand": "#C5AD8D",
-      "grey": "#74706D",
-      "cream": "#EFEDE8",
-      "black": "#231F20",
+      green: '#1F3D33',
+      olive: '#687A62',
+      brown: '#966241',
+      sand: '#C5AD8D',
+      grey: '#74706D',
+      cream: '#EFEDE8',
+      black: '#231F20',
     },
   },
-})
+});
 
 function MyApp({ Component, pageProps }) {
+  // initialize splitbee
+  splitbee.init();
+
   return (
     <ChakraProvider theme={theme}>
       <Box fontFamily={'Lora'}>
         <Component {...pageProps} />
       </Box>
     </ChakraProvider>
-  )
+  );
 }
 
 export default dynamic(() => Promise.resolve(MyApp), {
-    ssr: false,
+  ssr: false,
 });
